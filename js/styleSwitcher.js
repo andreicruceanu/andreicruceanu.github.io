757d4e349a9def3b798fee52fb303256a1a16ddf
andreicruceanu <82814224+andreicruceanu@users.noreply.github.com>
2022-03-09 11:52:13 +0200

    localStorage

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,14 +1,21 @@
 const links = document.querySelectorAll(".alternate-style");
 
+
 function setActiveStyle(color) {
   for (let i = 0; i < links.length; i++) {
     if (color === links[i].getAttribute("title")) {
       links[i].removeAttribute("disabled");
+      localStorage.setItem('color' , `${color}`)
     } else {
       links[i].setAttribute("disabled", "true");
     }
   }
 }
+
+if(localStorage.getItem('color') != null) {
+    setActiveStyle(localStorage.getItem('color'));
+}
+
 
 const toogleSwitcher = document.querySelector(".toogle-style-switcher");
 const styleSwitcher = document.querySelector(".style-switcher");
@@ -19,12 +26,27 @@ toogleSwitcher.addEventListener("click", () => {
 
 const bodySkin = document.querySelectorAll(".body-skin");
 
+
 for (let i = 0; i < bodySkin.length; i++) {
   bodySkin[i].addEventListener("change", function () {
     if (this.value === "Dark") {
-      document.body.classList.add("dark");
+      localStorage.setItem('darkMode' , "true")
+        document.body.classList.add("dark");
+      
     } else {
-      document.body.classList.remove("dark");
+      localStorage.setItem('darkMode' , "false")
+        document.body.classList.remove("dark");
     }
   });
 }
+if(localStorage.getItem('darkMode') == "false"){
+  document.body.classList.remove("dark");
+  document.getElementById("dark").checked = false ; 
+  document.getElementById("light").checked = true ;
+}
+if (localStorage.getItem('darkMode') == "true") {
+  document.body.classList.add("dark");
+  document.getElementById("dark").checked = true ; 
+  document.getElementById("light").checked = false ;
+} 
+
